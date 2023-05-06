85421d85b8794ec6cbe3246ee2e75e9ee90e21a4
2 <1241639333@qq.com>
2023-05-06 14:59:17 +0800

    prop

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,18 @@ import vue from '@vitejs/plugin-vue'
 
 import {join} from "path";
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
-    sourcemap: true
+    sourcemap:process.env.NODE_ENV=="development",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
   },
   resolve: {
     alias: {
