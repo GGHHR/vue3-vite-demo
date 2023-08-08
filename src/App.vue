76a18240ff60bbeb758aca590be20146431e29c3
2 <1241639333@qq.com>
2023-08-08 11:21:21 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
 import Head from "@/components/Head.vue";
+import {watch_router} from "@/assets/js/common";
+
+
+watch_router(function (newValue, oldValue) {
+    console.log(newValue, oldValue)
+})
 </script>
 
 <template>
   <Head></Head>
-    <router-view>  </router-view>
+  <router-view></router-view>
 </template>
 
 <style  lang="scss">
--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,6 +1,14 @@
 import {dayjs} from "element-plus";
+import router from "@/assets/router/router";
+import {watch} from "vue";
 
 export let formatter_datetime=function (row){
     const dateTime = dayjs(row);
     return dateTime.format('YYYY-MM-DD HH:mm:ss');
 }
+export let watch_router=function (fun){
+    watch(() =>router.currentRoute.value.path,(newValue,oldValue)=> {
+        fun(newValue,oldValue)
+    },{ immediate: true })
+}
+
