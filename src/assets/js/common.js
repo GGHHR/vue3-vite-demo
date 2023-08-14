6d4f635600ee2fd8ab044312b5818a00c9b2396e
2 <1241639333@qq.com>
2023-08-14 08:16:34 +0800

    1

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,6 +1,6 @@
 import {dayjs} from "element-plus";
 import router from "@/assets/router/router";
-import {watch} from "vue";
+import {watch,onBeforeUnmount} from "vue";
 
 export let formatter_datetime=function (row){
     const dateTime = dayjs(row);
@@ -12,3 +12,101 @@ export let watch_router=function (fun){
     },{ immediate: true })
 }
 
+export let  ws_fun=function (fun, wsUrl) {
+    let ws = null;
+    var lockReconnect = false;
+    createWebSocket(wsUrl);
+
+    function createWebSocket(url) {
+        try {
+            if ('WebSocket' in window) {
+                // ws= new WebSocket('ws://127.0.0.1:63423/plot/status?auth=123')
+                ws = new WebSocket(wsUrl)
+
+                /*setInterval(function(){
+                    console.log(1)
+                    ws.send('"keep"');
+                },1000)*/
+            }
+            initEventHandle();
+        } catch (e) {
+            reconnect(url);
+            console.log(e);
+        }
+    }
+
+    function initEventHandle() {
+
+        ws.onerror = function () {
+            reconnect(wsUrl);
+            console.log("llws连接错误!");
+        };
+        ws.onopen = function () {
+            heartCheck.reset().start();      //心跳检测重置
+            console.log("llws连接成功!" + new Date().toLocaleString());
+        };
+        ws.onmessage = function (event) {    //如果获取到消息，心跳检测重置
+
+
+            heartCheck.reset().start();      //拿到任何消息都说明当前连接是正常的
+            // console.log("llws收到消息啦:" +event.data);
+            if (!event.data.includes('ok')) {
+                //let data = jsON.parse(event.data);
+                fun(event)
+            }
+        };
+
+    }
+
+    // 监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
+    window.onbeforeunload = function () {
+        ws.close();
+    }
+
+    function reconnect(url) {
+        if (lockReconnect) return;
+        lockReconnect = true;
+        setTimeout(function () {     //没连接上会一直重连，设置延迟避免请求过多
+            createWebSocket(url);
+            lockReconnect = false;
+        }, 2000);
+    }
+
+    //心跳检测
+    var heartCheck = {
+        timeout: 1000,
+        timeoutObj: null,
+        serverTimeoutObj: null,
+        reset: function () {
+            clearTimeout(this.timeoutObj);
+            clearTimeout(this.serverTimeoutObj);
+            return this;
+        },
+        start: function () {
+            var self = this;
+            this.timeoutObj = setTimeout(function () {
+                //这里发送一个心跳，后端收到后，返回一个心跳消息，
+                //onmessage拿到返回的心跳就说明连接正常
+                try {
+                    if (ws.readyState == 1) {
+                        ws.send('"keep"');
+                        console.log("keep!")
+                    }
+                    self.serverTimeoutObj = setTimeout(function () {//如果超过一定时间还没重置，说明后端主动断开了
+                        ws.close();     //如果onclose会执行reconnect，我们执行ws.close()就行了.如果直接执行reconnect 会触发onclose导致重连两次
+                    }, self.timeout)
+                } catch (e) {
+
+                }
+
+
+            }, this.timeout)
+        }
+    }
+    onBeforeUnmount(() => {
+        ws.close();
+        ws = null;
+        clearTimeout(heartCheck.timeoutObj);
+        clearTimeout(heartCheck.serverTimeoutObj);
+    })
+}
